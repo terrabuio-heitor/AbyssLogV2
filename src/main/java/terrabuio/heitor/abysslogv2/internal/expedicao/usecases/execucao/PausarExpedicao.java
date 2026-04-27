0a6cf363568710ccedc61503a20545a0e683c5c3
heitor <heitorteerrabuio@gmail.com>
2026-04-27 11:52:45 -0300

    PausarExpedicao feito, como caso de uso

---
+++ src/main/java/terrabuio/heitor/abysslogv2/internal/expedicao/usecases/execucao/PausarExpedicao.java
@@ -0,0 +1,19 @@
+package terrabuio.heitor.abysslogv2.internal.expedicao.usecases.execucao;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import terrabuio.heitor.abysslogv2.internal.expedicao.domain.Expedicao;
+import terrabuio.heitor.abysslogv2.internal.expedicao.services.ExpedicaoService;
+
+@Service
+@RequiredArgsConstructor
+public class PausarExpedicao{
+    private final ExpedicaoService expedicaoService;
+    public void executar(Long Id){
+        Expedicao ex = expedicaoService.buscarPorId(Id);
+        if(ex.getStatus() != Expedicao.StatusExpedicao.ANDAMENTO){
+            throw new RuntimeException("Como se pode parar uma expedição que não está no mar???");
+        }
+        ex.setStatus(Expedicao.StatusExpedicao.PARADA);
+    }
+}
